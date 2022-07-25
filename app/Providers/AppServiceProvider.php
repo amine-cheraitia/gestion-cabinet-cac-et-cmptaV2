c34e8e813ec6785d3c0423b04ac0a1e1608027b6
Amine Cheraitia <amine-cheraitia@hotmail.com>
2022-07-25 02:06:24 +0200

    deploy v

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Carbon\Carbon;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,5 +28,9 @@ class AppServiceProvider extends ServiceProvider
         //Carbon::setLocale(config('app.locale'));
         Carbon::setLocale('fr');
         \Carbon\Carbon::setLocale('fr');
+
+        if(env('APP_ENV') !== 'local'){
+            URL::forceScheme('https');
+        }
     }
 }
